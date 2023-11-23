c42e1579e60853074cbe7c112e8700578e319de3
Abhishek S Kumar <abhishekskumar2002@gmail.com>
2023-11-23 15:10:16 +0530

    modified commit

--- steganography.py
+++ steganography.py
@@ -1,43 +1,54 @@
 import cv2
 import os
-import string
 
-img=cv2.imread("sukana.png")
-msg=input("Enter secret message: ")
-password=input("Enter a passcode: ")
+def generate_lookup_tables():
+    encrypt_table = {chr(i): i for i in range(256)}
+    decrypt_table = {i: chr(i) for i in range(256)}
+    return encrypt_table, decrypt_table
 
-d={}
-c={}
+def encrypt_image(img, message):
+    n, m, z = 0, 0, 0
+    for char in message:
+        img[n, m, z] = ord(char)
+        n += 1
+        m += 1
+        z = (z + 1) % 3
 
-for i in range (255):
-    d[chr(i)]=i
-    c[i]=chr(i)
+def decrypt_message(img):
+    n, m, z = 0, 0, 0
+    decrypted_message = ""
+    for _ in range(len(msg)):
+        decrypted_message += chr(img[n, m, z])
+        n += 1
+        m += 1
+        z = (z + 1) % 3
+    return decrypted_message
 
-n=0
-m=0
-z=0
+# Load the image
+img = cv2.imread("sukana.png")
+if img is None:
+    raise ValueError("Error loading the image.")
 
+# Encrypt the image
+msg = input("Enter secret message: ")
+encrypt_password = input("Enter encryption passcode:")
+encrypt_table, decrypt_table = generate_lookup_tables()
+encrypt_image(img, msg)
 
-for i in range(len(msg)):
-    img[n,m,z]=d[msg[i]]
-    n=n+1
-    m=m+1
-    z=(z+1)%3
-
-cv2.imwrite("encryptedImage.jpg",img)
+# Save the encrypted image
+cv2.imwrite("encryptedImage.jpg", img)
 os.startfile("encryptedImage.jpg")
-message=""
-n=0
-m=0
-z=0
 
-pas=input("Enter passcode for Decryption:")
-if(password==pas):
-    for i in range (len(msg)):
-        message=message+c[img[n,m,z]]
-        n=n+1
-        m=m+1
-        z=(z+1)%3
-    print ("Decrypted message = ",message)
+# Decrypt the message
+attempts = 3  # Set the maximum number of attempts
+for _ in range(attempts):
+    decrypt_password = input("Enter decryption passcode:")
+    if decrypt_password == encrypt_password:
+        decrypted_msg = decrypt_message(img)
+        print("Decrypted message =", decrypted_msg)
+        break
+    else:
+        print("Incorrect password. Please try again.")
 else:
-    print("You are not Authenticated")
+    print("Exceeded maximum attempts. Exiting.")
+    exit()
